807203eca1968c11cbf8cc521fededb11bbfae0a
Ryan Zegray <ryan.zegray@gmail.com>
2020-06-08 13:17:28 -0400

    Enable contributing an external jar
    Signed-off-by: Ryan Zegray <ryan.zegray@gmail.com>

--- src/languageServer/javaServerStarter.ts
+++ src/languageServer/javaServerStarter.ts
@@ -10,18 +10,18 @@ const DEBUG_PORT = 1064;
 const MICROPROFILE_SERVER_NAME = 'com.redhat.microprofile.ls-uber.jar';
 const MICROPROFILE_SERVER_MAIN_CLASS = 'com.redhat.microprofile.ls.MicroProfileServerLauncher';
 
-export function prepareExecutable(requirements: RequirementsData): Executable {
+export function prepareExecutable(requirements: RequirementsData, microprofileJavaExtensions: string[]): Executable {
   const executable: Executable = Object.create(null);
   const options: ExecutableOptions = Object.create(null);
   options.env = process.env;
   options.stdio = 'pipe';
   executable.options = options;
   executable.command = path.resolve(requirements.java_home + '/bin/java');
-  executable.args = prepareParams();
+  executable.args = prepareParams(microprofileJavaExtensions);
   return executable;
 }
 
-function prepareParams(): string[] {
+function prepareParams(microprofileJavaExtensions: string[]): string[] {
   const params: string[] = [];
   if (DEBUG) {
     params.push(`-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=${DEBUG_PORT},quiet=y`);
@@ -40,8 +40,13 @@ function prepareParams(): string[] {
   const serverHome: string = path.resolve(__dirname, '../server');
   const microprofileServerFound: Array<string> = glob.sync(`**/${MICROPROFILE_SERVER_NAME}`, { cwd: serverHome });
   if (microprofileServerFound.length) {
+    let mpJavaExtensionsClasspath = '';
+    if (microprofileJavaExtensions.length > 0) {
+      const classpathSeperator = os.platform() === 'win32' ? ';' : ':';
+      mpJavaExtensionsClasspath = classpathSeperator + microprofileJavaExtensions.join(classpathSeperator);
+    }
     params.push('-cp');
-    params.push(`${serverHome}/*`);
+    params.push(`${serverHome}/*` + mpJavaExtensionsClasspath);
     params.push(MICROPROFILE_SERVER_MAIN_CLASS);
   } else {
     throw new Error('Unable to find required Language Server JARs');
